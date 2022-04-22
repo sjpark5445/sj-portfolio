f1c818250dbd284e029b035cb9cba6b524ca8fcf
SeungJae Park <sjpark5445@gmail.com>
2022-04-22 11:20:27 +0900

    third commit

--- web_portfolio/js/portfolio.js
+++ web_portfolio/js/portfolio.js
@@ -24,23 +24,25 @@ $(function(){
     });
 
     // 팝업레이어
-    // let setModalNum;
-    // let getModalTitle = ['온양민속박물관', '새벽별 심리상담센터', '이디야 커피', '프렌즈 펭귄 아기물티슈'];
-    // let getModalSub = ['홍보물 디자인', '로고 디자인', '이벤트페이지', '제품 상세페이지'];
+    let setModalNum;
+    let getModalTitle = ['온양민속박물관', '새벽별 심리상담센터', 'EDIYA COFFEE', '프렌즈 펭귄 아기물티슈'];
+    let getModalSub = ['리플렛 디자인', '로고 디자인', '이벤트페이지', '제품 상세페이지'];
+    const getArticleNum = 4;
 
-    // $('.design .gallery').click(function(){
+    $('.design .txt a span').click(function(){
 
-    //     setModalNum = $(this).index();
-    //     function textView(){
-    //         $('.modal p').eq(setModalNum - 1).show().siblings('p').hide();
-    //     };
+        setModalNum = $(this).parent().parent().index() - getArticleNum;
+        console.log(setModalNum);
+        function textView(){
+            $('.modal p').eq(setModalNum - 1).show().siblings('p').hide();
+        };
         
-    //     $('.modal h3').text(getModalTitle[setModalNum - 1]);
-    //     $('.modal h5').text(getModalSub[setModalNum - 1]);
-    //     textView();
+        $('.modal h3').text(getModalTitle[setModalNum - 1]);
+        $('.modal h5').text(getModalSub[setModalNum - 1]);
+        textView();
 
-    //     $('.modal').addClass('modalOpen');
-    // });
+        $('.modal').addClass('modalOpen');
+    });
 
     $('#modalClose').click(function(){
         $('.modal').removeClass('modalOpen');
@@ -59,14 +61,4 @@ $(function(){
     setTimeout(function(){
         $('.title h4').css({opacity:1, top:'55%'});
     }, 500);
-
-    $('.circle1, .circle2').mouseover(function(){
-        let cirLeft = $(this).offset().left;
-
-        $(this).css({left:cirLeft - getScPer});
-    }).mouseout(function(){
-        let cirLeft = $(this).offset().left;
-
-        $(this).css({left:cirLeft + getScPer});
-    });
 });
